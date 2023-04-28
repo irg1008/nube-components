d6902e93b5f4831bd8ea1b34dca881e75ffc71b4
irg1008 <ivansudevlop@gmail.com>
2023-04-28 14:19:57 +0200

    💥 CHANGE: Change storybook root directory to tidy everything up

--- apps/storybook-react/storybook-static/assets/syntaxhighlighter-QTQ2UBB4-a15b5cb8.js
+++
@@ -1,2 +0,0 @@
-import{S as l,c as n,s as d}from"./index-fc8ff8aa.js";import"./iframe-8725b2d5.js";import"../sb-preview/runtime.mjs";import"./index-8db94870.js";import"./_commonjsHelpers-042e6b4d.js";import"./index-d475d2ea.js";import"./index-17f28e21.js";import"./index-d37d4223.js";import"./index-356e4a49.js";export{l as SyntaxHighlighter,n as createCopyToClipboardFunction,d as default};
-//# sourceMappingURL=syntaxhighlighter-QTQ2UBB4-a15b5cb8.js.map
