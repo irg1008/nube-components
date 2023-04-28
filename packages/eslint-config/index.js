2a2c0f87ff717fde12fe242b4e006cdf4b713ddd
irg1008 <ivansudevlop@gmail.com>
2023-04-28 11:42:57 +0200

    🎉 INT COMMIT: Start turborepo for both react and angular components

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: ['turbo', 'prettier', 'plugin:react/jsx-runtime'],
+  plugins: ['react'],
+  rules: {},
+};
---
+++ packages/ui-react/Button.tsx
@@ -0,0 +1,5 @@
+const Button = () => {
+  return <button>Boop</button>;
+};
+
+export { Button };
