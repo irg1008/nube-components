5c2dc9cb8d903ed1b322fcab36cde05f555077f2
irg1008 <ivansudevlop@gmail.com>
2023-05-02 14:15:59 +0200

    🚑 HOTFIX: Angular app now uses the ui library correctly with real time updated thanks to pnpm monorepo. Now the problem is tsconfig paths are not working

--- apps/storybook-angular/src/app/app.module.ts
+++ apps/storybook-angular/src/app/app.module.ts
@@ -4,15 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ButtonComponent } from 'ui-angular';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ButtonComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
