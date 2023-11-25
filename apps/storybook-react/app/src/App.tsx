6034b5a2340fdd9cd5021c306754e8be996dc9ed
irg1008 <ivansudevlop@gmail.com>
2023-11-25 03:55:17 +0100

    React/App: Modify code to import editor component with provider and styles

--- apps/storybook-react/app/src/App.tsx
+++ apps/storybook-react/app/src/App.tsx
@@ -1,9 +1,104 @@
-import { CustomEditor } from '@nubebytes/ui-react';
+import {
+  randFloat,
+  randProductCategory,
+  randProductName,
+  randUrl,
+} from '@ngneat/falso';
+import {
+  Editor,
+  EditorConfig,
+  Variable,
+  VariableConfig,
+} from '@nubebytes/ui-react';
+
+enum VariableType {
+  Price = 'price',
+  OfferPrice = 'offerPrice',
+  Name = 'name',
+  Image = 'image',
+  Image2 = 'image2',
+  Image3 = 'image3',
+  Image4 = 'image4',
+  Image5 = 'image5',
+  Category = 'category',
+}
+
+const imageVariables: Variable[] = [
+  {
+    label: 'Url Imagen',
+    key: VariableType.Image,
+  },
+  {
+    label: 'Url Imagen 2',
+    key: VariableType.Image2,
+  },
+  {
+    label: 'Url Imagen 3',
+    key: VariableType.Image3,
+  },
+  {
+    label: 'Url Imagen 4',
+    key: VariableType.Image4,
+  },
+  {
+    label: 'Url Imagen 5',
+    key: VariableType.Image5,
+  },
+];
+
+const textVariables: Variable[] = [
+  {
+    label: 'Precio',
+    key: VariableType.Price,
+  },
+  {
+    label: 'Precio de oferta',
+    key: VariableType.OfferPrice,
+  },
+  {
+    label: 'Nombre',
+    key: VariableType.Name,
+  },
+  {
+    label: 'Categoría',
+    key: VariableType.Category,
+  },
+];
+
+const price = randFloat({
+  min: 5,
+  max: 800,
+  fraction: 2,
+});
+
+const mockUp: Record<string, string> = {
+  [VariableType.Price]: price.toFixed(2),
+  [VariableType.OfferPrice]: (price * 0.8).toFixed(2),
+  [VariableType.Name]: randProductName(),
+  [VariableType.Category]: randProductCategory(),
+  [VariableType.Image]:
+    'https://i0.wp.com/contrapc.es/wp-content/uploads/2020/04/pacK-GAMING.png?fit=600%2C600&ssl=1',
+  [VariableType.Image2]: randUrl(),
+  [VariableType.Image3]: randUrl(),
+  [VariableType.Image4]:
+    'https://resource.logitech.com/w_800,c_lpad,ar_1:1,q_auto,f_auto,dpr_1.0/d_transparent.gif/content/dam/logitech/en/products/mice/m240/product-gallery/m240-mouse-top-view-graphite.png?v=1',
+  [VariableType.Image5]: randUrl(),
+};
+
+const variablesConfig: VariableConfig = {
+  textVariables,
+  imageVariables,
+  variableValueResolver: (k) => mockUp[k],
+};
+
+const editgorConfig: EditorConfig = {
+  variablesConfig,
+};
 
 export default function App() {
   return (
-    <>
-      <CustomEditor />
-    </>
+    <div className="h-screen w-full">
+      <Editor config={editgorConfig} />
+    </div>
   );
 }
