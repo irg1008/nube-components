be45f94f52e7f72fd2a5e463120fb91abe888ecf
irg1008 <ivansudevlop@gmail.com>
2023-11-25 03:58:10 +0100

    React/Config: Update build tsup and vite options as well as package json and new dependencies

---
+++ packages/ui-react/tailwind.config.mjs
@@ -0,0 +1,14 @@
+import withMT from '@material-tailwind/react/utils/withMT';
+import tailwindScrollbar from 'tailwind-scrollbar';
+import tailwindAnimated from 'tailwindcss-animated';
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    extend: {},
+  },
+  plugins: [tailwindScrollbar({ nocompatible: true }), tailwindAnimated],
+};
+
+export default withMT(config);
