b6532802dc68611cdbc6faa6e44708321e0f4fec
irg1008 <ivansudevlop@gmail.com>
2023-11-27 11:57:09 +0100

    (UI) React/Editor: Remove css var for sidebar width

--- packages/ui-react/src/components/editor/components/sidebar/index.tsx
+++ packages/ui-react/src/components/editor/components/sidebar/index.tsx
@@ -17,7 +17,7 @@ export const Sidebar = track(() => {
       className="absolute right-0 h-full w-0 overflow-visible lg:relative lg:w-auto">
       <section
         className={twMerge(
-          'relative h-full w-[calc(70vw)] max-w-[var(--sidebar-width)] gap-3 border-l-2 border-gray-900 bg-gray-50 shadow-xl transition-transform duration-300 lg:translate-x-0 lg:duration-0',
+          'relative h-full w-[calc(70vw)] max-w-xs gap-3 border-l-2 border-gray-900 bg-gray-50 shadow-xl transition-transform duration-300 lg:translate-x-0 lg:duration-0',
           open ? '-translate-x-full' : 'translate-x-0',
         )}>
         <span className="!absolute bottom-10 left-0 -translate-x-full p-6 lg:hidden">
